11a8fd7a049557f9e018a4289aa5a6dee1a97392
lonelycat <lonelycat@outlook.com>
2019-12-05 05:11:03 +0800

    Updates

---
+++ precache-manifest.2fb7a88bbcaad186d17aece864fc1c97.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f96893d939c29d8daf48f640712dd4",
+    "url": "/draggable-slides-2/index.html"
+  },
+  {
+    "revision": "6e214115b889587057ca",
+    "url": "/draggable-slides-2/static/js/2.f68f1e0f.chunk.js"
+  },
+  {
+    "revision": "749ef8f19eea67bd6257",
+    "url": "/draggable-slides-2/static/js/main.6a29c357.chunk.js"
+  },
+  {
+    "revision": "ea938015095b70951b5a",
+    "url": "/draggable-slides-2/static/js/runtime-main.c8fb7140.js"
+  }
+]);
