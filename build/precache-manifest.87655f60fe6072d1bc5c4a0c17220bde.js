cda1345345122cd103e09700803fd5c99dbba13b
lonelycat <lonelycat@outlook.com>
2019-12-05 00:19:59 +0800

    add build

---
+++ build/precache-manifest.87655f60fe6072d1bc5c4a0c17220bde.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07929fe3d671d34db1438bc1571df805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "562c40978b0826493ac0",
+    "url": "/static/js/2.a85a3fdb.chunk.js"
+  },
+  {
+    "revision": "749ef8f19eea67bd6257",
+    "url": "/static/js/main.6a29c357.chunk.js"
+  },
+  {
+    "revision": "28f05237b5de297c7e2e",
+    "url": "/static/js/runtime-main.a969c619.js"
+  }
+]);
